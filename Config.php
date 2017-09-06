b432f3b6f480f26e25533e9ef6e96d95af11de2a
zhumingzhen <itdream53@gmail.com>
2017-09-06 10:08:50 +0800

    hidden appid

--- Config.php
+++ Config.php
@@ -1,6 +1,6 @@
 <?php
 class Config{
-	const AppID = 'wxe6bd61546e337818';
-	const AppSecret = '1a6c491ccf0e0bb41641d0cfaec6e128';
+	const AppID = '公众号AppID';
+	const AppSecret = '公众号AppSecret';
 	const Debug = 1;
 }
